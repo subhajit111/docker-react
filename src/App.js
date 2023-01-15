d0268dc3d27bec4b51f63f19c620214fd81cfe55
subhajit111 <mishra.subhajit1998@gmail.com>
2023-01-15 21:33:38 +0530

    new branch added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          success
+          final success
         </p>
         <a
           className="App-link"
